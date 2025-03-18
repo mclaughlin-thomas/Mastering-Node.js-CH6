06729eecc3d5c293f390c28dfa949a1dba191a84
mclaughlin-thomas <thomas.mclaughlin2@stvincent.edu>
2025-03-18 11:01:05 -0400

    sending and downloading files to users

--- webapp/dist/server.js
+++ webapp/dist/server.js
@@ -5,16 +5,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const http_1 = require("http");
 const express_1 = __importDefault(require("express"));
-//import { basicHandler } from "./handler";
 const readHandler_1 = require("./readHandler");
 const port = 5000;
 const expressApp = (0, express_1.default)();
-// expressApp.get("/favicon.ico", (req, resp) => {
-//     resp.statusCode = 404;
-//     resp.end();
-// });
-//expressApp.get("*", basicHandler);
 expressApp.post("/read", readHandler_1.readHandler);
+expressApp.get("/sendcity", (req, resp) => {
+    resp.sendFile("city.png", { root: "static" });
+});
+expressApp.get("/downloadcity", (req, resp) => {
+    resp.download("static/city.png");
+});
+expressApp.get("/json", (req, resp) => {
+    resp.json("{name: Bob}");
+});
 //MIDDLEWARE BELOW
 expressApp.use(express_1.default.static("static"));
 // the middleware component will attempt to match request URLs
--- webapp/src/server.ts
+++ webapp/src/server.ts
@@ -1,17 +1,21 @@
 import { createServer } from "http";
-import express, {Express } from "express";
-//import { basicHandler } from "./handler";
+import express, {Express, Request, Response } from "express";
 import { readHandler } from "./readHandler";
 
 const port = 5000;
 const expressApp: Express = express();
 
-// expressApp.get("/favicon.ico", (req, resp) => {
-//     resp.statusCode = 404;
-//     resp.end();
-// });
-//expressApp.get("*", basicHandler);
 expressApp.post("/read", readHandler);
+expressApp.get("/sendcity", (req, resp) => {
+    resp.sendFile("city.png", { root: "static"});
+});
+expressApp.get("/downloadcity", (req: Request, resp: Response) => {
+    resp.download("static/city.png");
+});
+expressApp.get("/json", (req: Request, resp: Response) => {
+    resp.json("{name: Bob}");
+});
+
 //MIDDLEWARE BELOW
 expressApp.use(express.static("static"));
 // the middleware component will attempt to match request URLs
